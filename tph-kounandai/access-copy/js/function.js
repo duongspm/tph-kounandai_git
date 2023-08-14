dceca3c48e5f916139bcc97772a889da5519471e
duongspm <tranvanduong10a22016bb@gmail.com>
2023-08-14 17:02:21 +0700

    lụm

---
+++ tph-kounandai/access-copy/js/function.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $(document).on('submit', 'form#route', function () {
+        var start = $('input[name=start]').val();
+        var goal = $('input[name=goal]').val();
+        var e_start = encodeURIComponent(start);
+        var e_goal = encodeURIComponent(goal);
+        var gmapURL = 'http://maps.google.com/maps?saddr=' + e_start + '%e9%a7%85%2c%e6%97%a5%e6%9c%ac&daddr=' + e_goal + '%e9%a7%85&dirflg=r';
+
+        if (goal == "") {
+            alert('駅名を入力して下さい');
+        } else {
+            window.open(gmapURL, '_blank');
+        }
+
+        return false;
+    });
+});
+$(function(){
+    if ($(window).width() < 768) {
+        var scrollMap = $(".js-scroll__map");
+        var scrollMapImg = $(".js-scroll__map img");
+        
+        var mapTo = (scrollMapImg.width() - scrollMap.width()) / 2;
+        scrollMap.animate({scrollLeft: mapTo});
+    }
+});
--- tph-kounandai/life/js/function.js
+++ tph-kounandai/life/js/function.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1
                 }
             }
